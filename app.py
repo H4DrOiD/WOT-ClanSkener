e098ef95cedc9a8d2a6d6d008f7d4308d35e1c1b
H4DrOiD <marek.macay@gmail.com>
2025-04-16 06:42:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ def index():
         # TODO: spracovanie formulára
         return render_template('dashboard.html', players=[])
     return render_template('index.html')
-
 if __name__ == '__main__':
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
