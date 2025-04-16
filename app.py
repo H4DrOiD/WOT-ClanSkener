612d07be48633dd3073e84298758e4ef3c777ae0
H4DrOiD <marek.macay@gmail.com>
2025-04-16 09:09:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,11 @@ def index():
             battles_input = request.form.get('battles')
             email = request.form.get('email')
 
-            search_prefixes = ["a", "b", "c", "d", "e"]
+            search_prefixes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '-', '_', '.', '+', '=', '@']
             expected_values = load_expected_values()
             players = []
 
