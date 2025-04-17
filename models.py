1fc14456a33ce803f5dba3082928b7d5944f7fb7
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:08:25 +0200

    Update models.py

--- models.py
+++ models.py
@@ -3,10 +3,12 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class Commander(UserMixin, db.Model):
+class Commander(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(50), unique=True, nullable=False)
-    clan_name = db.Column(db.String(50), nullable=False)
+    nickname = db.Column(db.String(100), unique=True, nullable=False)
+    clan_name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    webhook_url = db.Column(db.String(300), nullable=True)
+    discord_webhook = db.Column(db.String(300), nullable=True)
 
+    def __repr__(self):
+        return f"<Commander {self.nickname}>"
