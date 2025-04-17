b5c31dc109e34dd1d061deb8515c44a37c2bcd2f
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:26:52 +0200

    Update wot_api.py

--- utils/wot_api.py
+++ utils/wot_api.py
@@ -1,37 +1,28 @@
 import requests
 import json
+import os
 
-WARGAMING_API_URL = "https://api.worldoftanks.eu/wot/account/"
-WARGAMING_API_KEY = '29b6e96e5fa1462cbebfb386fb565a0d'  # Zadaj svoj Wargaming API kľúč
+# API kľúč Wargaming
+WG_API_KEY = os.getenv('WG_API_KEY')  # Uisti sa, že tento kľúč je v environment variables
 
-def search_players_by_nickname(nickname):
-    url = f"{WARGAMING_API_URL}list/?application_id={WARGAMING_API_KEY}&search={nickname}"
-    response = requests.get(url)
-    return response.json()
-
-def get_account_info(account_id):
-    url = f"{WARGAMING_API_URL}info/?application_id={WARGAMING_API_KEY}&account_id={account_id}"
-    response = requests.get(url)
-    return response.json()
-
-def get_tank_stats(account_id):
-    url = f"https://api.worldoftanks.eu/wot/account/tanks/?application_id={WARGAMING_API_KEY}&account_id={account_id}"
-    response = requests.get(url)
-    return response.json()
-
-def calculate_wn8(account_id):
-    stats = get_account_info(account_id)
-    battles = stats.get("data", {}).get(str(account_id), {}).get("statistics", {}).get("all", {}).get("battles", 0)
-    wn8 = 0  # Tu bude tvoja implementácia výpočtu WN8, môžeš využiť externé knižnice alebo vlastný výpočet
-    return wn8
-
+# Funkcia na získanie štatistík hráča
 def get_player_stats(nickname):
-    player_data = search_players_by_nickname(nickname)
-    if player_data.get("status") == "ok":
-        player_info = player_data["data"]
-        if player_info:
-            account_id = player_info[0]["account_id"]
-            stats = get_account_info(account_id)
-            wn8 = calculate_wn8(account_id)
-            return stats, wn8
-    return None, None
+    url = f"https://api.worldoftanks.eu/wot/account/info/?application_id={WG_API_KEY}&search={nickname}"
+    response = requests.get(url)
+    data = response.json()
+
+    if data['status'] == 'ok' and len(data['data']) > 0:
+        player_data = data['data'].popitem()[1]  # Zoberieme prvého hráča zo zoznamu
+        player_name = player_data['nickname']
+        battles = player_data['statistics']['battles']
+        wn8 = player_data['statistics']['wn8']
+        
+        stats = {
+            "nickname": player_name,
+            "battles": battles,
+            "wn8": wn8,
+        }
+        
+        return stats, wn8
+    else:
+        return None, None
