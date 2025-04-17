cd7c1ef5a2a0cc204bb44bc0c4161750fe61f53f
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:28:30 +0200

    Update wot_api.py

--- utils/wot_api.py
+++ utils/wot_api.py
@@ -2,27 +2,33 @@ import requests
 import json
 import os
 
-# API kľúč Wargaming
-WG_API_KEY = os.getenv('WG_API_KEY')  # Uisti sa, že tento kľúč je v environment variables
+WARGAMING_API_KEY = os.getenv('WARGAMING_API_KEY')
+BASE_URL = "https://api.worldoftanks.eu/wot"
 
-# Funkcia na získanie štatistík hráča
 def get_player_stats(nickname):
-    url = f"https://api.worldoftanks.eu/wot/account/info/?application_id={WG_API_KEY}&search={nickname}"
-    response = requests.get(url)
-    data = response.json()
+    """
+    Získanie štatistík hráča z API World of Tanks.
+    """
+    url = f"{BASE_URL}/account/info/"
+    params = {
+        'application_id': WARGAMING_API_KEY,
+        'search': nickname
+    }
+    
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        player_data = data['data']
+        if nickname in player_data:
+            stats = player_data[nickname]
+            wn8 = calculate_wn8(stats)
+            return stats, wn8
+    return None, None
 
-    if data['status'] == 'ok' and len(data['data']) > 0:
-        player_data = data['data'].popitem()[1]  # Zoberieme prvého hráča zo zoznamu
-        player_name = player_data['nickname']
-        battles = player_data['statistics']['battles']
-        wn8 = player_data['statistics']['wn8']
-        
-        stats = {
-            "nickname": player_name,
-            "battles": battles,
-            "wn8": wn8,
-        }
-        
-        return stats, wn8
-    else:
-        return None, None
+def calculate_wn8(stats):
+    """
+    Vypočíta hodnotu WN8 podľa hráčskych štatistík.
+    """
+    # Tu môžeš pridať algoritmus na výpočet WN8 (hodnota WN8 sa počíta na základe rôznych faktorov ako presnosť, výhry, atď.)
+    wn8 = stats['battle_avg']['damage'] * 100  # Toto je iba príklad
+    return wn8
