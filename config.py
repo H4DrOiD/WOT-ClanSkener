420251245b09fa996eb7c20dc8bdf45d04eadc36
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:09:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "tajny_kluc_na_vyvoj")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///site.db")
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY') or 'tajny_kluc_pre_wot_skener'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
