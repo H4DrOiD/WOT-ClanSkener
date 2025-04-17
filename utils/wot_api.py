06a499c783390ce02ad8bc58a93588b9843704bf
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:29:45 +0200

    Update wot_api.py

--- utils/wot_api.py
+++ utils/wot_api.py
@@ -1,34 +1,39 @@
 import requests
-import json
 import os
 
-WARGAMING_API_KEY = os.getenv('WARGAMING_API_KEY')
-BASE_URL = "https://api.worldoftanks.eu/wot"
+# Definuj svoj API kľúč
+API_KEY = os.getenv("WARGAMING_API_KEY")
+BASE_URL = "https://api.worldoftanks.eu/wot/"
 
 def get_player_stats(nickname):
     """
-    Získanie štatistík hráča z API World of Tanks.
+    Získaj štatistiky hráča podľa jeho prezývky.
     """
-    url = f"{BASE_URL}/account/info/"
-    params = {
-        'application_id': WARGAMING_API_KEY,
-        'search': nickname
-    }
+    url = f"{BASE_URL}account/info/?application_id={API_KEY}&search={nickname}"
+    response = requests.get(url)
+    data = response.json()
     
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
+    if data.get('status') == 'ok':
         player_data = data['data']
         if nickname in player_data:
-            stats = player_data[nickname]
-            wn8 = calculate_wn8(stats)
+            player = player_data[nickname]
+            wn8 = calculate_wn8(player)
+            stats = {
+                'nickname': player['nickname'],
+                'battles': player['statistics']['battles'],
+                'wins': player['statistics']['wins'],
+                'wn8': wn8
+            }
             return stats, wn8
     return None, None
 
-def calculate_wn8(stats):
+def calculate_wn8(player_data):
     """
-    Vypočíta hodnotu WN8 podľa hráčskych štatistík.
+    Vypočíta hodnotu WN8 na základe štatistík hráča.
     """
-    # Tu môžeš pridať algoritmus na výpočet WN8 (hodnota WN8 sa počíta na základe rôznych faktorov ako presnosť, výhry, atď.)
-    wn8 = stats['battle_avg']['damage'] * 100  # Toto je iba príklad
-    return wn8
+    battles = player_data['statistics']['battles']
+    damage_dealt = player_data['statistics']['damage_dealt']
+    frags = player_data['statistics']['frags']
+    
+    wn8 = (damage_dealt * 0.5) + (frags * 0.3)  # Jednoduchý výpočet WN8
+    return round(wn8, 2)
