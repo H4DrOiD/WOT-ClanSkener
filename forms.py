dd38338ab4c7cd03e978b39db189087af43863b0
H4DrOiD <marek.macay@gmail.com>
2025-04-17 17:08:39 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, EqualTo, Length
 
-class RegisterForm(FlaskForm):
-    nickname = StringField('Prezývka', validators=[DataRequired(), Length(min=3, max=50)])
-    clan_name = StringField('Názov klanu', validators=[DataRequired(), Length(min=2, max=50)])
-    password = PasswordField('Heslo', validators=[DataRequired(), Length(min=6)])
-    submit = SubmitField('Registrovať')
+class RegistrationForm(FlaskForm):
+    nickname = StringField("Prezývka", validators=[DataRequired(), Length(min=2, max=100)])
+    clan_name = StringField("Názov klanu", validators=[DataRequired(), Length(min=2, max=100)])
+    password = PasswordField("Heslo", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Potvrď heslo", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Registrovať")
 
 class LoginForm(FlaskForm):
-    nickname = StringField('Prezývka', validators=[DataRequired()])
-    password = PasswordField('Heslo', validators=[DataRequired()])
-    submit = SubmitField('Prihlásiť sa')
+    nickname = StringField("Prezývka", validators=[DataRequired()])
+    password = PasswordField("Heslo", validators=[DataRequired()])
+    submit = SubmitField("Prihlásiť sa")
