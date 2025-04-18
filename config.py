1a47561154c9212020ec18bda0afadbc2afbd32d
H4DrOiD <marek.macay@gmail.com>
2025-04-18 13:51:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 
-# Wargaming API kľúč (získaný z developers.wargaming.net)
-WARGAMING_API_KEY = os.getenv("WARGAMING_API_KEY", "29b6e96e5fa1462cbebfb386fb565a0d")
+# Wargaming API kľúč
+WARGAMING_API_KEY = os.getenv("WARGAMING_API_KEY", "vloz_sem_svoj_kluc")
 
-# Discord webhook kam sa odošlú notifikácie
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL", "https://discordapp.com/api/webhooks/1361927609256116234/1DKfAyKhdw5aZ69hw2qoJLfNjP_mt0mlXLZTeHLeYrHer6CK3UIPCIDQ74nLm3m2ohhU")
+# Discord Webhook (dočasne testovací)
+DISCORD_WEBHOOK_URL = os.getenv(
+    "DISCORD_WEBHOOK_URL",
+    "https://discordapp.com/api/webhooks/1361927609256116234/1DKfAyKhdw5aZ69hw2qoJLfNjP_mt0mlXLZTeHLeYrHer6CK3UIPCIDQ74nLm3m2ohhU"
+)
+
+# Počet hráčov, ktorých chceme získať z API (limit)
+SEARCH_LIMIT = 100
+
+# Dostupné krajiny pre výber
+SUPPORTED_COUNTRIES = ["SK", "CZ", "PL", "HU", "UK"]
